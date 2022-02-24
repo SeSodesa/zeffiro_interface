79fc2168692391fc7d5052369b013341b8a184ad
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-02-24 13:02:24 +0200

    Improved documentation of stiffness matrix 𝐴

--- m/operators/zef_stiffness_matrix.m
+++ m/operators/zef_stiffness_matrix.m
@@ -1,16 +1,17 @@
 function A = zef_stiffness_matrix(basis, tensor)
 
-% The stiffness matrix 𝐴 of a discretized function 𝑢ₕ = ∑ᵢ𝑧ᵢψᵢ, with each 𝑧ᵢ
-% being a coordinate and ψᵢ a linear basis function, is defined by
+% The stiffness matrix 𝐴 of a discretized scalar function 𝑢ₕ = ∑ᵢ𝑧ᵢψᵢ, with
+% each 𝑧ᵢ being a coordinate and ψᵢ a linear basis function, is defined by
 %
 %   𝐴[i,j] = ∫[Ω] ∇ψⱼ ⋅ (𝑇∇ψᵢ) d𝑉 ,
 %
 % where Ω is the entire domain and d𝑉 is a finite volume element from which
-% the domain consists of, adn 𝑇 is a tensor, for which the equation
+% the domain consists of. Here 𝑇 is a tensor, for which the equation
 %
 %   (𝑇∇u)⋅𝑛⃗ = 0
 %
-% holds for the non-discretized function 𝑢, on the boundary ∂Ω of the domain.
+% holds with 𝑢 being the non-discretized scalar function 𝑢 on the boundary ∂Ω
+% of the domain and 𝑛⃗ is the surface normal on ∂Ω.
 
     error("Not yet implemented…")
 
