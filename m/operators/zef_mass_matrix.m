5b77ff2b011c7e09a65d8985e104e3929cc4e2a4
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-04-11 09:58:21 +0300

    Started writing m/operators/zef_mass_matrix.m
    This commit only adds the file, with comments on what it is supposed to do
    placed inside the function definition. The implementation comes later.

---
+++ m/operators/zef_mass_matrix.m
@@ -0,0 +1,9 @@
+function A = zef_mass_matrix()
+
+% zef_mass_matrix: a two-dimensional version of zef_stiffness_matrix which,
+% among other things, models the effects of any electrodes on the surface of a
+% head model Ω. Here the mass matrix A is given by ∫[𝑒ℓ] ψᵢψⱼ d𝑆, where each
+% 𝑒ℓ is an electrode with a surface area d𝑆,.and ψᵢ and ψⱼ are scalar-valued
+% basis functions of the diescretized domain Ω.
+
+end
