b2aed15d84c37e951f1027d53d8e6ad8b5bc5449
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-04-11 10:05:27 +0300

    zef_mass_matrix: added a missing ∑[𝑒ℓ…] to the function documentation comment
    This denotes summation over the electrodes 𝑒ℓ.

--- m/operators/zef_mass_matrix.m
+++ m/operators/zef_mass_matrix.m
@@ -2,8 +2,12 @@ function A = zef_mass_matrix()
 
 % zef_mass_matrix: a two-dimensional version of zef_stiffness_matrix which,
 % among other things, models the effects of any electrodes on the surface of a
-% head model Ω. Here the mass matrix A is given by ∫[𝑒ℓ] ψᵢψⱼ d𝑆, where each
-% 𝑒ℓ is an electrode with a surface area d𝑆,.and ψᵢ and ψⱼ are scalar-valued
-% basis functions of the diescretized domain Ω.
+% head model Ω. Here the mass matrix A is given by
+%
+%   ∑[𝑒ℓ=1,𝐿] 1 / (𝑍ℓ |𝑒ℓ|) ⋅ ∫[𝑒ℓ] ψᵢψⱼ d𝑆,
+%
+% where each 𝑒ℓ is an electrode with a surface area d𝑆, 𝑍ℓ is the impedance of
+% an electrode, and ψᵢ and ψⱼ are scalar-valued basis functions of the
+% discretized domain Ω.
 
 end
