3aef7e82a2411d0737cb65cf4c19f42a8b7ab9bb
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-05-18 16:06:29 +0300

    zef_source_model_from: added more robustness, like handling of ZefSourceModels idempotently

--- m/zef_source_model_from.m
+++ m/zef_source_model_from.m
@@ -3,11 +3,37 @@ function source_model = zef_source_model_from(input)
     % A function that creates an instance of the enumeration ZefSourceModel
     % based on given input.
 
-    KNOWN_INTEGERS = [1, 2];
-
     % Initialize value as invalid
 
     source_model = ZefSourceModel.Error;
+
+    % Check that we receive input.
+
+    if ~ nargin == 1
+        warning('I accept exactly one input.')
+        return
+    end
+
+    % Function is idempotent with respect to given ZefSourceModel variants.
+
+    if isenum(input)
+        switch input
+            case ZefSourceModel.Error
+                source_model = input;
+            case ZefSourceModel.Whitney
+                source_model = input;
+            case ZefSourceModel.Hdiv
+                source_model = input;
+            otherwise
+                warning("I received an enum that is not a ZefSourceModel. Setting erraneous return value.")
+                source_model = ZefSourceModel.Error;
+        end
+
+        return
+
+    end
+
+    KNOWN_INTEGERS = [1, 2];
 
     % Check for valid inputs.
 
