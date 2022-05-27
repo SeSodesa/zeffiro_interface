8c69c1fec90e647231abf29897efbc5bd80d3cb5
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-05-27 09:39:45 +0300

    zef_L2_norm: added a missing semicolon

--- m/zef_L2_norm.m
+++ m/zef_L2_norm.m
@@ -2,6 +2,6 @@ function norm = zef_L2_norm(vec, dim)
 
     % Calculates the L2-norm of a given array arr along a given dimension dim.
 
-    norm = sqrt(sum(arr, dim))
+    norm = sqrt(sum(arr, dim));
 
 end
