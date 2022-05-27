8c4b1d613be7b69ebd9e2fa7cac511bd4e4192e0
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-05-27 09:48:26 +0300

    zef_L2_norm: add check for 2 input arguments

--- m/zef_L2_norm.m
+++ m/zef_L2_norm.m
@@ -1,7 +1,13 @@
 function norm = zef_L2_norm(vec, dim)
 
     % Calculates the L2-norm of a given array arr along a given dimension dim.
+    % If the dimension is not given, calculates the norm assuming arr is a
+    % vector.
 
-    norm = sqrt(sum(arr, dim));
+    if nargin == 2
+        norm = sqrt(sum(arr.^2, dim));
+    else
+        norm = sqrt(sum(arr.^2));
+    end
 
 end
