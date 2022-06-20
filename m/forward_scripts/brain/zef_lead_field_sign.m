346f036cada05f0c0a81a7de06403f2e81f3b728
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-06-20 14:09:28 +0300

    zef_lead_field_sign: renamed parameters and added argument validations

--- m/forward_scripts/brain/zef_lead_field_sign.m
+++ m/forward_scripts/brain/zef_lead_field_sign.m
@@ -1,31 +1,31 @@
 function Lsign = zef_lead_field_sign( ...
-    source_positions, ...
-    electrodes, ...
-    L ...
+    p_source_positions, ...
+    p_electrode_positions, ...
+    p_L ...
 )
 
-    % Calculates the sign of the given lead field L based on source positions,
+    % Calculates the sign of the given lead field p_L based on source positions,
     % their directions, the electrode positions and the lead field itself.
 
     arguments
-        source_positions (:, 3) double
-        electrodes
-        L
+        p_source_positions (:, 3) double
+        p_electrode_positions (:, 3) double
+        p_L
     end
 
     % Source closest to origin (SCO).
 
-    source_distances = zef_L2_norm(source_positions, 2);
+    source_distances = zef_L2_norm(p_source_positions, 2);
 
     [~, scoind] = min(source_distances);
 
-    scopos = source_positions(scoind,:);
+    scopos = p_source_positions(scoind,:);
 
     % Electrode whose projection onto the xy-plane is closest to that of SCO,
     % as in is "most below/above it". Call it projectively closest electrode
     % (PCE).
 
-    sco_ele_diffs = scopos - electrodes;
+    sco_ele_diffs = scopos - p_electrode_positions;
 
     sco_ele_diffs_xy = sco_ele_diffs(:, 1:2);
 
@@ -33,13 +33,13 @@ function Lsign = zef_lead_field_sign( ...
 
     [~, pceind] = min(pce_dists);
 
-    % Indices of L that match the SCO.
+    % Indices of p_L that match the SCO.
 
     sco_xyz_inds = 3 * (scoind-1) + 1 : 3 * scoind;
 
-    % Part of L that matches both SCO and PCE.
+    % Part of p_L that matches both SCO and PCE.
 
-    scopceL = L(pceind, sco_xyz_inds);
+    scopceL = p_L(pceind, sco_xyz_inds);
 
     % Scalar potential of SCO at EGZ.
 
