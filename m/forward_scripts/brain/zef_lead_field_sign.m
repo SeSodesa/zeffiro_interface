540a6da860fa40fc3aa5cbe8430d21d957f515bf
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-06-20 12:39:58 +0300

    zef_lead_field_sign: change comparison logic
    Instead of comparing source closest to origin (SCO) and electrode with largest
    z-coordinate, compare the SCO and electrode whose xy-projection is closest to
    that of SCO.

--- m/forward_scripts/brain/zef_lead_field_sign.m
+++ m/forward_scripts/brain/zef_lead_field_sign.m
@@ -21,28 +21,36 @@ function Lsign = zef_lead_field_sign( ...
 
     scopos = source_positions(scoind,:);
 
-    % Electrode with greatest z-coordinate (EGZ)
+    % Electrode whose projection onto the xy-plane is closest to that of SCO,
+    % as in is "most below/above it". Call it projectively closest electrode
+    % (PCE).
 
-    [~, egzind] = max(electrodes(:,3));
+    sco_ele_diffs = scopos - electrodes;
 
-    % Indices of L that match the SCO
+    sco_ele_diffs_xy = sco_ele_diffs(:, 1:2);
+
+    pce_dists = zef_L2_norm(sco_ele_diffs_xy, 2);
+
+    [~, pceind] = min(pce_dists);
+
+    % Indices of L that match the SCO.
 
     sco_xyz_inds = 3 * (scoind-1) + 1 : 3 * scoind;
 
-    % Part of L that matches both SCO and EGZ
+    % Part of L that matches both SCO and PCE.
 
-    scoegzL = L(egzind, sco_xyz_inds);
+    scopceL = L(pceind, sco_xyz_inds);
 
-    % Scalar potential of SCO at EGZ
+    % Scalar potential of SCO at EGZ.
 
-    scoegzu = scoegzL * scopos';
+    scopceu = scopceL * scopos';
 
     % Calculate the sign
 
     try
-        Lsign = sign(scoegzL(3) / scoegzu);
+        Lsign = sign(scopceL(3) / scopceu);
     catch
-        warning('Sign of L could not be determined because of zero division. Setting it as (+)')
+        warning('Sign of L could not be determined because of zero division. Setting it as (+)');
         Lsign = 1;
     end
 
