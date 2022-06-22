636a5ac46ca426ec0c4561c80973048b915093f3
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-06-22 14:40:20 +0300

    zef_deep_tetra: continued writing

--- m/zef_deep_tetra.m
+++ m/zef_deep_tetra.m
@@ -1,7 +1,8 @@
-function o_tetra = zef_deep_tetra( ...
-    p_nodes, ...
-    p_tetra, ...
-    p_volume_inds ...
+function out_tetra = zef_deep_tetra( ...
+    in_nodes, ...
+    in_tetra, ...
+    in_volume_inds, ...
+    in_acceptable_depth_mm ...
 )
 
     % Produces subset of the FEM tetrahedra, whose barycenters are deep enough
@@ -10,9 +11,12 @@ function o_tetra = zef_deep_tetra( ...
     % Input:
     %
     % - p_nodes: finite elements nodes.
-    % - p_tetra: finite element tetrahedra constructed from above nodes.
+    % - p_tetra: finite element tetrahedra (quadruples of node indices)
+    %   constructed from above nodes.
     % - p_volume_inds: the indices of the tetrahedra that form the volume
-    %   under observation
+    %   under observation.
+    % - p_acceptable_depth_mm: the depth in millimetres, within which the deep
+    %   tetra are located.
     %
     % Output:
     %
@@ -20,9 +24,33 @@ function o_tetra = zef_deep_tetra( ...
     %   segment.
 
     arguments
-        p_nodes (:,3) double
-        p_tetra (:,4) double {mustBeInteger, mustBePositive}
-        p_volume_inds (:,1) double {mustBeInteger, mustBePositive}
+        in_nodes (:,3) double
+        in_tetra (:,4) double {mustBeInteger, mustBePositive}
+        in_volume_inds (:,1) double {mustBeInteger, mustBePositive}
+        in_acceptable_depth_mm (1,1) double {mustBeNonnegative}
     end
+
+    % Set empty return value.
+
+    out_tetra = [];
+
+    % First find out the surface triangles (triples of node indices) of the
+    % volume determined by the volume indices.
+
+    volume_tetra = in_tetra(in_volume_inds, :);
+
+    volume_surface_triangles = zef_surface_mesh(volume_tetra);
+
+    % An abbreviation variable for clearer indexing.
+
+    vst = volume_surface_triangles;
+
+    % Calculate the normed surface normals of the triangles.
+
+    triangle_edges_1 =  in_nodes(vst(:, 2), :) - in_nodes(vst(:, 1), :);
+    triangle_edges_2 =  in_nodes(vst(:, 3), :) - in_nodes(vst(:, 1), :);
+
+    surface_normals = cross(triangle_edges_1, triangle_edges_2);
+    surface_normals = surface_normals ./ zef_L2_norm(surface_normals, 2);
 
 end
