51f0f76bfe9bad62d26319a89c660fb9e0b04983
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-07-01 10:03:43 +0300

    zef_kron_reduction: source model taken into consideration

--- m/forward_scripts/brain/zef_kron_reduction.m
+++ m/forward_scripts/brain/zef_kron_reduction.m
@@ -17,6 +17,7 @@ function out_reduced_interpolation_matrix = zef_kron_reduction( ...
     % - in_interpolation_matrix: the matrix being reduced.
     %
     % - in_schur_compement: this is applied to in_interpolation_matrix to
+    %   reduce its sturcture.
     %
     % - in_electrode_model: if this is not 'CEM' but 'PEM', the matrix is
     %   returned as-is. Has to be one of these options.
@@ -30,21 +31,36 @@ function out_reduced_interpolation_matrix = zef_kron_reduction( ...
         in_interpolation_matrix
         in_schur_complement
         in_electrode_model { mustBeText, mustBeMember(in_electrode_model, {'CEM', 'PEM'}) }
+        in_source_model { mustBeA(in_source_model, ["ZefSourceModel"]) }
     end
 
     out_reduced_interpolation_matrix = in_interpolation_matrix;
 
     schur_size = size(in_schur_complement);
 
-    if isequal(in_electrode_model,'CEM')
+    if strcmp(in_electrode_model,'CEM')
 
-        inv_schur_complement = in_schur_complement \ eye(schur_size);
+        switch in_source_model
 
-        out_reduced_interpolation_matrix = ...
-            inv_schur_complement ...
-            * ...
-            in_interpolation_matrix ...
-        ;
+            case { ZefSourceModel.Whitney, ZefSourceModel.Hdiv }
 
+                inv_schur_complement = in_schur_complement \ eye(schur_size);
+
+                out_reduced_interpolation_matrix = ...
+                    inv_schur_complement ...
+                    * ...
+                    in_interpolation_matrix ...
+                ;
+
+            case ZefSourceModel.StVenant
+
+                % Do nothing. TODO: check whether St. Venant should also
+                % trigger the reduction.
+
+            otherwise
+
+                error("Unknown source model. Should be one of ZefSourceModel.{Whitney, Hdiv, StVenant}");
+
+        end
     end
 end
