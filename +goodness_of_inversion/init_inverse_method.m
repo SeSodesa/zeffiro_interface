770519094092dd98942b68472f36ceb3eb0be6b6
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-09-23 14:31:42 +0300

    init_inverse_method: initialize some of the needed zef fields for MNE and Beamformer

--- +goodness_of_inversion/init_inverse_method.m
+++ +goodness_of_inversion/init_inverse_method.m
@@ -1,14 +1,85 @@
-function zef = zef_init_inverse_method(zef, inverse_method_name, mne, beamformer, ramus, sesame)
+function zef = init_inverse_method(zef, inverse_method_name, mne, beamformer, ramus, sesame)
 
-    % See if a given inverse method is recognised and if so, call it after
-    % running the required initialization script.
+    % init_inverse_method
+    %
+    % Set required values in zef based on the given inverse method name and
+    % related given arguments.
+    %
+
+    arguments
+
+        zef struct
+
+        inverse_method_name (1,1) string { mustBeMember(inverse_method_name, ["MNE", "Beamformer", "RAMUS", "SESAME"]) }
+
+        mne struct
+
+        beamformer struct
+
+        ramus struct
+
+        sesame struct
+
+    end
 
     if strcmp(inverse_method_name, "MNE")
-        % No need to do anything for MNE.
+
+        zef.mne_low_cut_frequency = mne.mne_low_cut_frequency;
+
+        zef.mne_high_cut_frequency = mne.mne_high_cut_frequency;
+
+        zef.mne_normalize_data = mne.mne_normalize_data;
+
+        zef.mne_number_of_frames = mne.mne_number_of_frames;
+
+        zef.mne_prior = mne.mne_prior;
+
+        zef.mne_sampling_frequency = mne.mne_sampling_frequency;
+
+        zef.mne_time_start = mne.mne_time_start;
+
+        zef.mne_time_window = mne.mne_time_window;
+
+        zef.mne_time_step = mne.mne_time_step;
+
     end
 
     if strcmp(inverse_method_name, "Beamformer")
-        % zef_init_beamformer;
+
+        zef.beamformer_estimate_type = beamformer.beamformer_estimate_type;
+
+        zef.beamformer_estimated_attribute = beamformer.beamformer_estimated_attribute;
+
+        zef.beamformer_covariance_regularization_parameter = beamformer.beamformer_covariance_regularization_parameter;
+
+        zef.beamformer_covariance_mode = beamformer.beamformer_covariance_mode;
+
+        zef.beamformer_lead_field_regularization_parameter = beamformer.beamformer_lead_field_regularization_parameter;
+
+        zef.beamformer_lead_field_regularization_procedure = beamformer.beamformer_lead_field_regularization_procedure;
+
+        zef.beamformer_signal_to_noise_ratio = beamformer.beamformer_signal_to_noise_ratio;
+
+        zef.beamformer_sampling_frequency = beamformer.beamformer_sampling_frequency;
+
+        zef.beamformer_low_cut_frequency = beamformer.beamformer_low_cut_frequency;
+
+        zef.beamformer_high_cut_frequency = beamformer.beamformer_high_cut_frequency;
+
+        zef.beamformer_time_start = beamformer.beamformer_time_start;
+
+        zef.beamformer_time_window = beamformer.beamformer_time_window;
+
+        zef.beamformer_n_of_time_steps = beamformer.beamformer_n_of_time_steps;
+
+        zef.beamformer_time_step = beamformer.beamformer_time_step;
+
+        zef.beamformer_data_segment = beamformer.beamformer_data_segment;
+
+        zef.beamformer_data_normalization = beamformer.beamformer_data_normalization;
+
+        zef.beamformer_lead_field_normalization = beamformer.beamformer_lead_field_normalization;
+
     end
 
     if strcmp(inverse_method_name, "RAMUS")
